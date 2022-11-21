45d10e0f5f1470c16e4c7f7f95dc1b02d06ca640
BlueKanary <gwynnek@yahoo.com>
2022-11-21 13:17:30 -0500

    Added main.js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Azure-Website
+
 Codebase for website deployment
+
+Based on https://github.com/madebygps/cgc-azure-resume-starter.git
---
+++ frontend/main.js
@@ -0,0 +1,21 @@
+// Website by Kaiden Gwynne
+
+Window.addEventListiner('DOMContentLoaded', (event) =>{
+    getVisitorCount();
+})
+
+const functionApi = '';
+
+const getVisitorCount = () => {
+    let count = -1;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response => {
+        console.log("Function API was called");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
